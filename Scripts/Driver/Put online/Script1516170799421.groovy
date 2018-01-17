31423d4fe4ee487f76d674477d32c73eebb0ef6c
Pasindu <Pasindu@DESKTOP-41D70DC>
2018-01-17 12:40:16 +0530

    update

---
+++ Scripts/Driver/Put online/Script1516170799421.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('C:\\Users\\Pasindu\\Desktop\\Avonet\\drive-download-20180104T074633Z-001\\app-driver.apk', true)
+
+Mobile.tap(findTestObject('Driver/Sign In/android.view.ViewGroup6'), 0)
+
+Mobile.setText(findTestObject('Driver/Sign In/android.view.ViewGroup4'), '0766500626', 0)
+
+Mobile.setText(findTestObject('Driver/Sign In/android.view.ViewGroup6 (1)'), 'Dilan6370', 0)
+
+Mobile.tap(findTestObject('Driver/Sign In/android.view.ViewGroup9'), 0)
+
+Mobile.tap(findTestObject('Driver/Sign In/android.view.ViewGroup9 (1)'), 0)
+
+Mobile.tap(findTestObject('Driver/Put Online/android.view.ViewGroup5'), 0)
+
+Mobile.tap(findTestObject('Driver/Put Online/android.widget.Switch0 - OFF'), 0)
+
+Mobile.tap(findTestObject('Driver/Put Online/android.widget.TextView6 - Online'), 0)
+
